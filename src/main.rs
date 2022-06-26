17cf7fd042d45c349e8b8bd8265a6e6eb77c7f8d
noamsauerutley <noamsauerutley@gmail.com>
2022-06-26 13:24:08 -0400

    Add random name pull functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+
+use rand::seq::SliceRandom;
+
 fn main() {
-    println!("Hello, world!");
+    let names: [&str; 10] = ["A---", "A--", "B----", "D-", "D---", "F------", "J--", "L-----", "M---", "N---"];
+    let name = match names.choose(&mut rand::thread_rng()) {
+        Some(n) => {n}
+        None => {"oops"}
+    };
+    println!("{}", name)
 }
